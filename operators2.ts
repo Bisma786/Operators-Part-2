73c97c87714c021bf731f9172bc5266bd36753ea
Bisma Gondal <159830145+Bisma786@users.noreply.github.com>
2024-03-05 10:30:07 +0500

    Add files via upload

---
+++ operators2.ts
@@ -0,0 +1,46 @@
+console.log("Assignmnet Operators");
+let b=3;
+b +=4;
+console.log(b)
+
+console.log("Comparison Operators");
+let c=3;
+let d=4;
+let e=7;
+let f=7;
+
+let isEqualnot= (c==f); //false
+console.log(isEqualnot);
+
+let isNotEqual = (d!=e);//true
+console.log(isNotEqual)
+
+let isEquals =(e==f);//true
+console.log(isEquals);
+
+let Equal =(f!=e); //false
+console.log(Equal);
+
+let Greaterthan = (e>c); //true
+console.log(Greaterthan);
+
+let Lessthan =(f<d); //false
+console.log(Lessthan);
+
+console.log("Logical Operators");
+let g=10;
+let h=5;
+
+let LogicalAnd = (g>h) && (h>0);//true
+console.log(LogicalAnd)
+
+let LogicalOr =(g<0) ||( g>h);//true
+console.log(LogicalOr);
+
+let LogicalNot = !(h>0 ) && !(g>h);//false
+console.log(LogicalNot);
+
+
+
+
+
